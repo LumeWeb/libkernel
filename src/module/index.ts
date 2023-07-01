e6a13a16cce277f3782526ac9aef3160e8f613e4
Derrick Hammer <derrick@derrickhammer.com>
2023-07-01 03:03:44 -0400

    fix: export handlePresentKey

--- src/module/index.ts
+++ src/module/index.ts
@@ -1,6 +1,6 @@
 export { log, logErr } from "./log.js";
 export { ActiveQuery, addHandler, handleMessage } from "./messages.js";
 export { callModule, connectModule, newKernelQuery } from "./queries.js";
-export { getDataFromKernel, getKey } from "./key.js";
+export { getDataFromKernel, getKey, handlePresentKey } from "./key.js";
 export { moduleQuery, presentKeyData } from "./types.js";
 export { Client, factory } from "./client.js";
