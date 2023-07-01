94287ce2d9472c820e0f776aa391f09f488bf448
Derrick Hammer <derrick@derrickhammer.com>
2023-07-01 05:08:56 -0400

    refactor: re-export several crypto, string, and binary utilities

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-export { DataFn, Err, addContextToErr, objAsString } from "@lumeweb/libweb";
+export {
+  DataFn,
+  Err,
+  ErrTuple,
+  addContextToErr,
+  objAsString,
+  hexToBuf,
+  b64ToBuf,
+  bufToB64,
+  bufToStr,
+  decodeU64,
+  encodeU64,
+  ed25519,
+  sha512,
+  ensureBytes,
+  equalBytes,
+  utf8ToBytes,
+} from "@lumeweb/libweb";
 export { callModule, connectModule, log, logErr } from "./api.js";
