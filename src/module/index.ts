2ac16d2c5c6f505d4a76d41c5e9e3ad67dacb1cd
Derrick Hammer <derrick@derrickhammer.com>
2023-07-23 13:29:25 -0400

    feat: re-export defer from p-defer

--- src/module/index.ts
+++ src/module/index.ts
@@ -1,3 +1,6 @@
+import defer from "p-defer";
+export { defer };
+
 export { log, logErr } from "./log.js";
 export { ActiveQuery, addHandler, handleMessage } from "./messages.js";
 export { callModule, connectModule, newKernelQuery } from "./queries.js";
