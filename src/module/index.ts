55b578aaceaea0ae92700fccbb7eca5085c6b35c
Derrick Hammer <derrick@derrickhammer.com>
2023-07-23 13:36:34 -0400

    fix: export NetworkClient

--- src/module/index.ts
+++ src/module/index.ts
@@ -7,3 +7,7 @@ export { callModule, connectModule, newKernelQuery } from "./queries.js";
 export { getDataFromKernel, getKey, handlePresentKey } from "./key.js";
 export { moduleQuery, presentKeyData } from "./types.js";
 export { Client, factory } from "./client.js";
+
+import NetworkClient from "./clients/network.js";
+
+export { NetworkClient };
