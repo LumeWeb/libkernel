cb5a6175f648130b2ffdb57bdeaa0af0ec3e9529
Derrick Hammer <derrick@derrickhammer.com>
2023-09-11 06:14:32 -0400

    fix: export login and serviceWorkerReady

--- src/kernel/index.ts
+++ src/kernel/index.ts
@@ -3,7 +3,15 @@ export {
   loginComplete,
   logoutComplete,
   openAuthWindow,
+  login,
 } from "./auth.js";
 export { kernelVersion } from "./query/version.js";
-export { callModule, connectModule, init, newKernelQuery } from "./queries.js";
+export {
+  callModule,
+  connectModule,
+  init,
+  newKernelQuery,
+  getKernelIframe,
+  serviceWorkerReady,
+} from "./queries.js";
 export { log, logErr } from "./log.js";
